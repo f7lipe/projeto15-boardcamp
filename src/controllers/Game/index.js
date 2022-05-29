bfc78a017120a5aea74dd6b8e6f155d0e545b4aa
= <f7lipe@me.com>
2022-05-29 18:06:50 -0300

    feat: set game

--- src/controllers/Game/index.js
+++ src/controllers/Game/index.js
@@ -35,4 +35,34 @@ async function getGames(req, res) {
     }
 }
 
-export { getGames }
+async function setGame(req, res){
+    try {
+        const game = req.body
+        const validation = gameSchema.validate(game)
+        if (validation.error) return res.sendStatus(400)
+    
+        const query = await connection.query(`SELECT id
+                                               FROM categories 
+                                               WHERE id=$1`, [game.categoryId])
+
+        if (query.rowCount === 0) return res.sendStatus(400)
+    
+        await connection.query(`
+          INSERT INTO 
+            games(name, image, "stockTotal", "categoryId", "pricePerDay")
+          VALUES ($1, $2, $3, $4, $5);
+        `, [game.name, 
+            game.image, 
+            Number(game.stockTotal), 
+            game.categoryId, 
+            Number(game.pricePerDay)]);
+    
+        res.sendStatus(201)
+
+      } catch (error) {
+        res.sendStatus(500)
+        console.log(chalk.red(error))
+      }
+}
+
+export { getGames, setGame }
--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,9 +1,10 @@
 
 import { Router } from "express";
-import { getGames } from "../../controllers/Game";
+import { getGames, setGame } from "../../controllers/Game";
 
 const gameRouter = Router()
 
 gameRouter.get('/games', getGames)
+gameRouter.post('/games', setGame)
 
 export {gameRouter}
