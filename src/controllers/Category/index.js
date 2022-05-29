70288ba32e63e59fb0baf4a09bb9867001b40b01
= <f7lipe@me.com>
2022-05-29 14:51:50 -0300

    feat: setCategory

--- src/controllers/Category/index.js
+++ src/controllers/Category/index.js
@@ -13,6 +13,31 @@ import chalk from "chalk";
     }
   }
   
+   async function setCategory(req, res) {
+    try {
+      const category = req.body;
+      const validation = categorySchema.validate(category);
+      if (validation.error) {
+        return res.sendStatus(400);
+      }
+  
+      const result = await connection.query(`SELECT id 
+                                             FROM categories 
+                                             WHERE name=$1`, 
+                                             [category.name]);
+      if (result.rowCount > 0) {
+        return res.sendStatus(409);
+      }
+  
+      await connection.query(`INSERT INTO categories(name) 
+                              VALUES ($1)`, 
+                              [category.name]);
+  
+      res.sendStatus(201);
+    } catch (error) {
+      res.sendStatus(500);
+      console.log(chalk.bold.red("Deu ruim para fazer a operação ", error))
+    }
+  }
 
-
-  export {getCategories}
+  export {getCategories, setCategory}
--- src/routes/Catergory/index.js
+++ src/routes/Catergory/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getCategories } from "../../controllers/Category";
+import { getCategories, setCategory } from "../../controllers/Category";
 
 const categoryRouter = Router()
 categoryRouter.get('/categories', getCategories)
-
+categoryRouter.post('/categories', setCategory)
 
 export {categoryRouter}
